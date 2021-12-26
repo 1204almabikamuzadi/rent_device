867890a2a47df3cc5ba0b4147293a7c1a1b274c9
1204almabikamuzadi <cizubualfred@yahoo.fr>
2021-12-26 23:04:05 +0100

    cart mgtmt

--- ei_2021_alfred/resources/js/components/helpers/Pict.js
+++ ei_2021_alfred/resources/js/components/helpers/Pict.js
@@ -4,10 +4,14 @@ const handleDefault=(e)=>{
     e.target.src="http://127.0.0.1:8000/avatar/default.jpg"
    
 }
+const mystyle = {
+    width:'50px',
+    height:'50px'
+  };
 
 function Pict(props) {
     return (
-        <img src={props.src} onError={handleDefault} alt="" />
+        <img src={props.src} style={mystyle} onError={handleDefault} alt="" />
     )
 }
 
